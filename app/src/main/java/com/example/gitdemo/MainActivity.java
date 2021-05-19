d8ee421a24b22a57a2ef4c3103e1bae4a178e0d2
李鑫熠 <2466758288@qq.com>
2021-05-19 11:47:40 +0800

    修复：旋转导致计数归零

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.gitdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.security.Key;
+
 public class MainActivity extends AppCompatActivity {
+    private static String NUMBER = "number";
     private int number = 0;
     private Button mButtonAdd, mButtonSub;
     private TextView mTextView;
@@ -18,10 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mButtonAdd = findViewById(R.id.buttonAdd);
         mButtonSub = findViewById(R.id.buttonSub);
+
+        number = savedInstanceState!=null ?savedInstanceState.getInt(NUMBER):0;
+
         mTextView = findViewById(R.id.textview);
         mTextView.setText(String.valueOf(number));
         mButtonAdd.setOnClickListener(v -> mTextView.setText(String.valueOf(++number)));
         mButtonSub.setOnClickListener(v -> mTextView.setText(String.valueOf(--number)));
+
+
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NUMBER,number);
 
     }
 }
