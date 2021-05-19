e7edc97d8bc10df6fb1a57f499703c4c1f44d131
李鑫熠 <2466758288@qq.com>
2021-05-19 10:03:40 +0800

    实现了button按钮的+1功能

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int number = 0;
+    private Button mButtonAdd;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonAdd = findViewById(R.id.buttonAdd);
+        mTextView = findViewById(R.id.textview);
+        mTextView.setText(String.valueOf(number));
+        mButtonAdd.setOnClickListener(v -> mTextView.setText(String.valueOf(++number)));
+
     }
 }
