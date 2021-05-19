24420210b283d9f32d616cc7685a9f056126c9b0
李鑫熠 <2466758288@qq.com>
2021-05-19 10:35:53 +0800

    实现了button按钮的-1功能

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int number = 0;
-    private Button mButtonAdd;
+    private Button mButtonAdd ,mButtonSub;
     private TextView mTextView;
 
     @Override
@@ -17,9 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButtonAdd = findViewById(R.id.buttonAdd);
+        mButtonSub = findViewById(R.id.buttonSub);
         mTextView = findViewById(R.id.textview);
         mTextView.setText(String.valueOf(number));
         mButtonAdd.setOnClickListener(v -> mTextView.setText(String.valueOf(++number)));
+        mButtonSub.setOnClickListener(v -> mTextView.setText(String.valueOf(--number)));
 
     }
 }
